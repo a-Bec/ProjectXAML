d802fd703024ccb7f444b42522eb76c8cd548b86
Arwed Beck <arwed.beck@gmail.com>
2023-02-10 21:02:03 +0100

    First entry and try with XAML

--- ProjectXAML/MainWindow.xaml.cs
+++ ProjectXAML/MainWindow.xaml.cs
@@ -23,6 +23,12 @@ namespace ProjectXAML
         public MainWindow()
         {
             InitializeComponent();
+            CloseButton.Click += CloseButton_Click;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
