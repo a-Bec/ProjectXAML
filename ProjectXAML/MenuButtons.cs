2712bc5ca2047ef70ee039c4b903312c2ecc4584
Arwed Beck <arwed.beck@gmail.com>
2023-03-19 13:45:03 +0100

    new Playerclass added

--- ProjectXAML/MenuButtons.cs
+++ ProjectXAML/MenuButtons.cs
@@ -1,20 +1,36 @@
-﻿namespace ProjectXAML
+﻿using System.Windows;
+
+namespace ProjectXAML
 {
     internal class MenuButtons
     {
         public void Start()
         {
+            try
+            {
 
+            }
+            catch
+            {
+
+            }
         }
 
         public void Load()
         {
+            try
+            {
 
+            }
+            catch
+            {
+
+            }
         }
 
         public void Close()
         {
-
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ ProjectXAML/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.Dynamic;
+
+namespace ProjectXAML
+{
+    internal class Player
+    {
+        string name = "player";
+        string color;
+        int score = 0;
+
+
+
+        public string setName()
+        {
+            return name;
+        }
+
+        public int setScore( int score )
+        {
+            return score++;
+        }
+        public string setColor()
+        {
+            return color;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public int getScore()
+        {
+            return score;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+        public void movePlayer()
+        {
+
+        }
+
+    }
+}
