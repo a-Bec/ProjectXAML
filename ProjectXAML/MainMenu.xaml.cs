a649db1efd1850043339b66fc3e10ba1d8004bc1
Arwed Beck <arwed.beck@gmail.com>
2023-03-21 15:32:31 +0100

    MainWindow rebuild with pages

---
+++ ProjectXAML/MainMenu.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectXAML
+{
+    /// <summary>
+    /// Interaktionslogik für MainMenu.xaml
+    /// </summary>
+    ///
+    public partial class MainMenu : Page
+    {
+        MenuButtons menu = new MenuButtons();
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NameBox nameBox= new NameBox();
+                MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+
+                mainWindow.MainFrame.NavigationService.Navigate(nameBox);
+
+                int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+                int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+                int score = player.getScore();
+                player.setPosition(screenHeight, screenWidth);
+            }
+            catch
+            {
+
+            }
+        }
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            menu.Load();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            menu.Close();
+        }
+    }
+}
--- ProjectXAML/MainWindow.xaml.cs
+++ ProjectXAML/MainWindow.xaml.cs
@@ -6,44 +6,16 @@ namespace ProjectXAML
 {
     public partial class MainWindow : Window
     {
-        bool menuVisibility = true;
-        MenuButtons menu = new MenuButtons();
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void StartButton_Click( object sender, RoutedEventArgs e )
+        private void onLoad(object sender, RoutedEventArgs e)
         {
-            changeMenuVisibility( menuVisibility );
-            menu.Start();
-        }
-        private void LoadButton_Click( object sender, RoutedEventArgs e )
-        {
-            menu.Load();
+            MainMenu mainMenu= new MainMenu();
+            MainFrame.Navigate(mainMenu);
         }
 
-        private void ExitButton_Click( object sender, RoutedEventArgs e )
-        {
-            menu.Close();
-        }
-
-        private void changeMenuVisibility( bool menuVisibility )
-        {
-            if( menuVisibility )
-            {
-            StartButton.Visibility = Visibility.Collapsed;
-            LoadButton.Visibility = Visibility.Collapsed;
-            ExitButton.Visibility = Visibility.Collapsed;
-            menuVisibility = false;
-            }
-            else
-            {
-                StartButton.Visibility = Visibility.Visible;
-                LoadButton.Visibility = Visibility.Visible;
-                ExitButton.Visibility = Visibility.Visible;
-            }
-        }
     }
 }
